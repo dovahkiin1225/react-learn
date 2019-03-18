ecee6fc95fbdd7dc929f31dd47e7060cb10b0cf2
yosion <yosion.peng@qq.com>
2019-03-18 22:57:27 +0800

    feat:引入了react的代理

--- antd-course/config/config.js
+++ antd-course/config/config.js
@@ -30,4 +30,13 @@ export default {
             dva: true
         }],
     ],
+    // 把 ajax 请求发送到你的本地开发服务器，然后本地开发服务器再把 ajax 请求转发到远端去，
+    // 从网络拓扑上看本地开发服务器起着「反向代理」的作用。
+    // 本地服务器和远端服务器是「服务器和服务器间的通信」，就不存在跨域问题了
+    proxy: {
+        '/dev': {
+            target: 'https://08ad1pao69.execute-api.us-east-1.amazonaws.com',
+            changeOrigin: true,
+        },
+    },
 };
