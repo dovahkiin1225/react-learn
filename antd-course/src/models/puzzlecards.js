d67a3e33f9c2bd87092313ab069e891e0b792a50
pengyx <pengyx@szinfinova.com>
2019-03-18 19:03:08 +0800

    delay:回家接着搞

--- antd-course/src/models/puzzlecards.js
+++ antd-course/src/models/puzzlecards.js
@@ -1,19 +1,34 @@
+// import request from '../util/request';  
+// import request from 'https://08ad1pao69.execute-api.us-east-1.amazonaws.com';  
+// request 是 demo 项目脚手架中提供的一个做 http 请求的方法，是对于 fetch 的封装，返回 Promise
+
+const delay = (millisecond) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, millisecond);
+    });
+};
+
 export default {
     namespace: 'puzzlecards',
     state: {
-        data: [
-          {
-            id: 1,
-            setup: 'Did you hear about the two silk worms in a race?',
-            punchline: 'It ended in a tie',
-          },
-          {
-            id: 2,
-            setup: 'What happens to a frog\'s car when it breaks down?',
-            punchline: 'It gets toad away',
-          },
-        ],
-        counter: 100,
+        data: [],
+        counter: 0,
+    },
+    effects:{
+        *queryInitCards(_, sagaEffects){
+            const { call, put} = sagaEffects;
+            const endPointURI = 'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke';
+
+            const puzzle = yield call(request, endPointURI);        // 获取服务端数据。
+            yield put({ type:'addNewCard', payload: puzzle});       
+                // 添加一个卡片数据。这个会触发 reducer 的执行。于是会看到视图上添加了一个新卡片。
+
+            yield call(delay, 3000);
+
+            const puzzle2 = yield call(request,endPointURI);        // 第二次获取服务端数据
+            yield put({ type: 'addNewCard', payload: puzzle2});     
+                // 再添加一个卡片数据。这个又会触发 reducer 的执行。于是看到第二个卡片添加到视图上去。
+        }
     },
     reducers: {     // reducers: 用来响应 action 并修改 state
         addNewCard(state, { payload: newCard }) {
--- antd-course/src/page/puzzlecards.js
+++ antd-course/src/page/puzzlecards.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { Card,Button  } from 'antd';
+import { Card/* ,Button */  } from 'antd';
 
 import { connect } from 'dva';
 
 const namespace = 'puzzlecards';
 
 const mapStateToProps = (state) => {
-    const cardList = state[namespace];
+    const cardList = state[namespace].data;
     return {
         cardList,
     };
@@ -21,50 +21,24 @@ const mapDispatchToProps = (dispatch) => {
             };
             dispatch(action);
         },
+        onDidMount: () => {
+            dispatch({
+                type: `${namespace}/queryInitCards`,
+            });
+        },
     };
 };
 
 @connect(mapStateToProps, mapDispatchToProps)
 export default class PuzzleCardsPage extends Component{
-    /* constructor(props){
-        super();
-        this.state = {
-            cardList: [
-                {
-                    id: 1,
-                    setup: 'Did you hear about the two silk worms in a race?',
-                    punchline: 'It ended in a tie',
-                },
-                {
-                    id: 2,
-                    setup: 'What happens to a frog\'s car when it breaks down?',
-                    punchline: 'It gets toad away',
-                },
-            ],
-        }
+    componentDidMount() {
+        this.props.onDidMount();
     }
-
-    addNewCard = () => {
-        this.setState(prevState => {
-            const prevCardList = prevState.cardList;
-            this.counter += 1;
-            const card = {
-                id: this.counter,
-                setup: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
-                punchline: 'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            };
-            return {
-                cardList: prevCardList.concat(card),
-            };
-        });
-    } */
-
     render() {
         return (
             <div>
                 {
-                    // this.state.cardList.map(card => {
-                    this.props.cardList.data.map(card => {
+                    this.props.cardList.map(card => {
                         return (
                             <Card key={card.id}>
                                 <div>Q: {card.setup}</div>
@@ -78,14 +52,14 @@ export default class PuzzleCardsPage extends Component{
                 {/* <div>
                     <Button onClick={this.addNewCard}> 添加卡片 </Button>
                 </div> */}
-                <div>
+                {/* <div>
                     <Button onClick={() => this.props.onClickAdd({
                         setup: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
                         punchline: 'here we use dva',
                     })}> 
                         添加卡片 
                     </Button>
-                </div>
+                </div> */}
             </div>
         );
     }
