7063ef665f07a60c7a3da5e1e2ae11ce41dc4372
pengyx <pengyx@szinfinova.com>
2019-03-19 10:25:37 +0800

    feat:使用mock来模拟后台数据

--- antd-course/src/models/puzzlecards.js
+++ antd-course/src/models/puzzlecards.js
@@ -2,6 +2,9 @@ import request from '../util/request';
 // import request from 'https://08ad1pao69.execute-api.us-east-1.amazonaws.com';  
 // request 是 demo 项目脚手架中提供的一个做 http 请求的方法，是对于 fetch 的封装，返回 Promise
 
+// 错误捕获
+import { message } from 'antd';
+
 const delay = (millisecond) => {
     return new Promise((resolve) => {
         setTimeout(resolve, millisecond);
@@ -16,23 +19,30 @@ export default {
     },
     effects:{
         *queryInitCards(_, sagaEffects){
-            const { call, put} = sagaEffects;
-            // const endPointURI = 'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke';
-            // const endPointURI = 'http://10.82.13.243:3005/random_joke';      
-            const endPointURI = '/random_joke';         // 使用的代理，重定向
-            // const endPointURI = '/amazonaws/random_joke';         // 使用的代理，重定向
-            // const endPointURI = '/baidu/random_joke';         // 使用的代理，重定向
-            // const endPointURI = '/404/random_joke';         // 使用的代理，重定向
+            try { // 加入 try catch 捕获抛错
 
-            const puzzle = yield call(request, endPointURI);        // 获取服务端数据。
-            yield put({ type:'addNewCard', payload: puzzle});       
-                // 添加一个卡片数据。这个会触发 reducer 的执行。于是会看到视图上添加了一个新卡片。
+                const { call, put} = sagaEffects;
+                // const endPointURI = 'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke';
+                // const endPointURI = 'http://10.82.13.243:3005/random_joke';      
+                // const endPointURI = '/random_joke';         // 使用的代理，重定向
+                const endPointURI = 'dev/random_joke';         // 使用的代理，重定向
+                // const endPointURI = '/amazonaws/random_joke';         // 使用的代理，重定向
+                // const endPointURI = '/baidu/random_joke';         // 使用的代理，重定向
+                // const endPointURI = '/404/random_joke';         // 使用的代理，重定向
 
-            yield call(delay, 3000);
+                const puzzle = yield call(request, endPointURI);        // 获取服务端数据。
+                yield put({ type:'addNewCard', payload: puzzle});       
+                    // 添加一个卡片数据。这个会触发 reducer 的执行。于是会看到视图上添加了一个新卡片。
 
-            const puzzle2 = yield call(request,endPointURI);        // 第二次获取服务端数据
-            yield put({ type: 'addNewCard', payload: puzzle2});     
-                // 再添加一个卡片数据。这个又会触发 reducer 的执行。于是看到第二个卡片添加到视图上去。
+                yield call(delay, 3000);
+
+                const puzzle2 = yield call(request,endPointURI);        // 第二次获取服务端数据
+                yield put({ type: 'addNewCard', payload: puzzle2});     
+                    // 再添加一个卡片数据。这个又会触发 reducer 的执行。于是看到第二个卡片添加到视图上去。
+
+            } catch (e) {
+                message.error('数据获取失败'); // 打印错误信息
+              }
         }
     },
     reducers: {     // reducers: 用来响应 action 并修改 state
