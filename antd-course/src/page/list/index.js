e2648cabb3e9f6ee53c1e74cf53c70a2fd9519f5
yosion <yosion.peng@qq.com>
2019-03-20 23:13:07 +0800

    ✨ Feat: 表单

--- antd-course/src/page/list/index.js
+++ antd-course/src/page/list/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-import { Table } from 'antd';
+import { Table, Modal, Button, Form, Input } from 'antd';
 
 // connect 是连接 dva 和 React 两个平行世界的关键
 import { connect } from 'dva';
 
+const FormItem = Form.Item;
 const namespace = 'cards';
 
 function mapStateToProps(state) {
@@ -15,6 +16,35 @@ function mapStateToProps(state) {
 }
 
 class List extends React.Component {
+  state = {
+    visible: false,
+  };
+
+  showModal = () => {
+    this.setState({ visible: true });
+  };
+
+  handleCancel = () => {
+    this.setState({
+      visible: false,
+    });
+  }
+
+  handleOk = () => {
+    const { dispatch, form: { validateFields } } = this.props;
+  
+    validateFields((err, values) => {
+      if (!err) {
+        dispatch({
+          type: 'cards/addOne',
+          payload: values,
+        });
+        // 重置 `visible` 属性为 false 以关闭对话框
+        this.setState({ visible: false });
+      }
+    });
+  }
+
   componentDidMount() {
     this.props.dispatch({
       type: `${namespace}/queryList`,
@@ -38,11 +68,43 @@ class List extends React.Component {
   ];
 
   render() {
+    const { visible } = this.state;
+    const { form: { getFieldDecorator } } = this.props;
     const { cardsList, cardsLoading } = this.props;
   
     return (
       <div>
         <Table columns={this.columns} dataSource={cardsList} loading={cardsLoading} rowKey="id" />
+        <Button onClick={this.showModal}>新建</Button>
+
+        <Modal title="新建记录" 
+          visible={visible}
+          visible={visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+          <Form>
+            <FormItem label="名称">
+              {getFieldDecorator('name', {
+                rules: [{ required: true }],
+              })(
+                <Input />
+              )}
+            </FormItem>
+            <FormItem label="描述">
+              {getFieldDecorator('desc')(
+                <Input />
+              )}
+            </FormItem>
+            <FormItem label="链接">
+              {getFieldDecorator('url', {
+                rules: [{ type: 'url' }],
+              })(
+                <Input />
+              )}
+            </FormItem>
+          </Form>
+        </Modal>
       </div>
     );
   }
@@ -50,6 +112,7 @@ class List extends React.Component {
   
   // ...
 }
-export default connect(mapStateToProps)(List);
+// export default connect(mapStateToProps)(List);
+export default connect(mapStateToProps)(Form.create()(List));
 
 // export default List;
